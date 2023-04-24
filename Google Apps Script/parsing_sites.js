e05974f1f1895874be1e821e267ac9aac6a8d9b0
Mikhail Shardin <43311539+empenoso@users.noreply.github.com>
2023-04-24 15:55:42 +0500

    :magic_wand: updated. it works

--- Google Apps Script/parsing_sites.js
+++ Google Apps Script/parsing_sites.js
@@ -7,19 +7,31 @@
  * @author Mikhail Shardin [Михаил Шардин] 
  * @site https://shardin.name/
  * 
- * Last updated: 19.02.2022
+ * Last updated: 21.04.2023
  * 
  */
 
+function testTinkoff() {
+    var url = 'https://journal.tinkoff.ru/list/i-am-smart/';
+    VCBR = journal_tinkoff_ru(url)
+}
+
 function journal_tinkoff_ru(url) {
-    Utilities.sleep(7 * 1000) // 15 sec
     try {
         var html = UrlFetchApp.fetch(url).getContentText();
-        let Views = +html.match(/,"views":(.*?),"favoritesCount":/)[1] 
-        let Comments = +html.match(/"stats":{"comments":(.*?),"/)[1]
-        let Bookmarks = +html.match(/,"favoritesCount":(.*?)}/)[1] // +html.match(/<span class="_1T6f2">(.*?)<\/span><\/button>/)[1]
-        let Ratings = `?` // +html.match(/<span class="_sRng z2yqt">(.*?)<\/span><button type="button" class="_1bWSy _1eR8l"/)[1];        
-
+        var searchstring = 'articleUUID":"'
+        var index = html.search(searchstring);
+        if (index >= 0) {
+            var pos = index + searchstring.length
+            var articleUUID = html.substring(pos, pos + 36)
+            Logger.log(`articleUUID: https://journal.tinkoff.ru/api/public/v1/potoque/?uuid=${articleUUID}`) // https://social.journal.tinkoff.ru/api/v25/profiles/229963/articles/
+        }
+        const response = UrlFetchApp.fetch(`https://journal.tinkoff.ru/api/public/v1/potoque/?uuid=${articleUUID}`)
+        const json = JSON.parse(response.getContentText());
+        let Views = json.data[0].viewsCount
+        let Comments = json.data[0].commentsCount
+        let Bookmarks = json.data[0].favoritesCount
+        let Ratings = json.data[0].likesCount
         Logger.log(`Для ${url}:\nПросмотры = ${Views} \nКомментарии = ${Comments} \nЗакладки = ${Bookmarks} \nРейтинг = ${Ratings}.`)
         return `${Views}|${Comments}|${Bookmarks}|${Ratings}`
     } catch (error) {
@@ -40,7 +52,7 @@ function youtube_com(url) {
         Views = stat.viewCount
         Comments = '-'
         Bookmarks = '-'
-        Ratings = "+" + stat.likeCount + " / -?" //+ stat.dislikeCount
+        Ratings = "+" + stat.likeCount //+ " / -?" //+ stat.dislikeCount
 
         Logger.log(`Для ${url}:\nПросмотры = ${Views} \nКомментарии = ${Comments} \nЗакладки = ${Bookmarks} \nРейтинг = ${Ratings}.`)
         return `${Views}|${Comments}|${Bookmarks}|${Ratings}`
@@ -50,26 +62,40 @@ function youtube_com(url) {
     }
 }
 
+function testHabr() {
+    var url = 'https://habr.com/post/481566/';
+    VCBR = habr_com(url)
+}
+
 function habr_com(url) {
     // try {
-        var html = UrlFetchApp.fetch(url).getContentText();
-        let Views = +html.match(/<span class="tm-icon-counter__value">(.*?)K<\/span>/)[1]
-            .replace(/\,/g, '.') * 1000
-        // let Comments = `?`// +html.match(/class="tm-comments__comments-count">(.*?)<\/span>/)[1]
-        var searchstringComments = '       Комментарии '
-        var index = html.search(searchstringComments);
-        if (index >= 0) {
-            var pos = index + searchstringComments.length
-            var Comments = html.substring(pos, pos + 70)
-            Comments = +Comments
-                .split('</span>')[0]
-                .replace(/<\/sp/g, '');
-            (!Comments || Comments === undefined) ? Comments = 0: Comments
-        }
-        let Bookmarks = `?` // +html.match(/<span title="Количество пользователей, добавивших публикацию в закладки" class="bookmarks-button__counter">(.*?)<\/span>/)[1]
-        let Ratings = +html.match(/class="tm-votes-meter__value tm-votes-meter__value_positive tm-votes-meter__value_appearance-article tm-votes-meter__value_rating">(.*?)<\/span>/)[1]
-        Logger.log(`Для ${url}:\nПросмотры = ${Views} \nКомментарии = ${Comments} \nЗакладки = ${Bookmarks} \nРейтинг = ${Ratings}.`)
-        return `${Views}|${Comments}|${Bookmarks}|${Ratings}`
+    var html = UrlFetchApp.fetch(url).getContentText();
+    // Logger.log(`html:\n${html}.`)
+    let Views = +html.match(/<span class="tm-icon-counter__value">(.*?)K<\/span>/)[1]
+        .replace(/\,/g, '.') * 1000
+    // let Comments = `?`// +html.match(/class="tm-comments__comments-count">(.*?)<\/span>/)[1]
+    var searchstringComments = '       Комментарии '
+    var index = html.search(searchstringComments);
+    if (index >= 0) {
+        var pos = index + searchstringComments.length
+        var Comments = html.substring(pos, pos + 70)
+        Comments = +Comments
+            .split('</span>')[0]
+            .replace(/<\/sp/g, '');
+        (!Comments || Comments === undefined) ? Comments = 0: Comments
+    }
+
+    var searchstringBookmarks = 'bookmarks-button__counter'
+    var index = html.search(searchstringBookmarks);
+    if (index >= 0) {
+        var pos = index + searchstringBookmarks.length
+        var Bookmarks = html.substring(pos, pos + 20)
+        Bookmarks = +Bookmarks.match(/\d{1,4}/);
+        (!Bookmarks || Bookmarks === undefined) ? Bookmarks = 0: Bookmarks
+    }
+    let Ratings = +html.match(/tm-votes-meter__value_appearance-article tm-votes-meter__value_rating\">(.*?)<\/span>/)[1]
+    Logger.log(`Для ${url}:\nПросмотры = ${Views} \nКомментарии = ${Comments} \nЗакладки = ${Bookmarks} \nРейтинг = ${Ratings}.`)
+    return `${Views}|${Comments}|${Bookmarks}|${Ratings}`
     // } catch (error) {
     //     Logger.log(`Ошибка чтения данных для ${url}.`)
     //     return `?|?|?|?`
@@ -94,24 +120,30 @@ function github_com(url) {
     }
 }
 
+function testTG() {
+    var url = 'https://t.me/google_sheets/464';
+    VCBR = t_me(url)
+}
+
 function t_me(url) {
+    // try {
     urlReplace = url
         .replace(/t\.me/g, 'tgstat.ru/channel/@')
         .replace(/\/\@\//g, '/@')
     Logger.log(`Старый адрес: ${url}.\nНовый адрес: ${urlReplace}.`)
-    try {
-        var html = UrlFetchApp.fetch(urlReplace).getContentText();
-        Views = +html.match(/<span class="fa fa-eye"><\/span>&nbsp;(.*?)k            <\/a>/)[1] * 1000
-        Comments = '-'
-        Bookmarks = '-'
-        Ratings = '-'
 
-        Logger.log(`Для ${url}:\nПросмотры = ${Views} \nКомментарии = ${Comments} \nЗакладки = ${Bookmarks} \nРейтинг = ${Ratings}.`)
-        return `${Views}|${Comments}|${Bookmarks}|${Ratings}`
-    } catch (error) {
-        Logger.log(`Ошибка чтения данных для ${url}.`)
-        return `?|?|?|?`
-    }
+    var html = UrlFetchApp.fetch(urlReplace).getContentText();
+    Views = +html.match(/<i class=\"uil-eye\"><\/i>(.*?)k/)[1] * 1000
+    Comments = '-' // +html.match(/<i class=\"uil-comments-alt\"><\/i>(.*?)                    <\/span>/)[1]
+    Bookmarks = '-'
+    Ratings = +html.match(/<i class=\"uil-share-alt\"><\/i>(.*?)                <\/a>/)[1]
+
+    Logger.log(`Для ${url}:\nПросмотры = ${Views} \nКомментарии = ${Comments} \nЗакладки = ${Bookmarks} \nРейтинг = ${Ratings}.`)
+    return `${Views}|${Comments}|${Bookmarks}|${Ratings}`
+    // } catch (error) {
+    //     Logger.log(`Ошибка чтения данных для ${url}.`)
+    //     return `?|?|?|?`
+    // }
 }
 
 function d3today_ru(url) {
@@ -130,13 +162,18 @@ function d3today_ru(url) {
     }
 }
 
+function testVC() {
+    var url = 'https://vc.ru/finance/92990-upravlencheskiy-uchet-lichnyh-aktivov';
+    VCBR = vc_ru(url)
+}
+
 function vc_ru(url) {
     try {
         var html = UrlFetchApp.fetch(url).getContentText();
-        Views = +html.match(/<span class="views__value">(.*?)<\/span>/)[1]
-        Comments = +html.match(/<span class="comments_counter__count__value">(.*?)<\/span>/)[1]
+        Views = +html.match(/<span class=\"views__value\">(.*?)<\/span>/)[1]
+        Comments = '-' // +html.match(/<span class="comments_counter__count__value">(.*?)<\/span>/)[1]
         Bookmarks = '-'
-        Ratings = +html.match(/<span class="vote__value__v vote__value__v--real">(.*?)<\/span>/)[1]
+        Ratings = '-' //+html.match(/<span class="vote__value__v vote__value__v--real">(.*?)<\/span>/)[1]
 
         Logger.log(`Для ${url}:\nПросмотры = ${Views} \nКомментарии = ${Comments} \nЗакладки = ${Bookmarks} \nРейтинг = ${Ratings}.`)
         return `${Views}|${Comments}|${Bookmarks}|${Ratings}`
@@ -145,25 +182,3 @@ function vc_ru(url) {
         return `?|?|?|?`
     }
 }
-
-function testTiktok() {
-    var url = 'https://www.tiktok.com/@di.maiers/video/6938025243042794753';
-    var html = UrlFetchApp.fetch(url, {
-        muteHttpExceptions: true
-    });
-    title = html.getContentText().match(/<title>(.*?)<\/title>/)[1]
-
-    Logger.log(`Для ${url}: название = ${title}.`)
-    return `${title}`
-}
-
-function testHabr() {
-    var url = 'https://habr.com/ru/post/645935/';
-    VCBR = habr_com(url)
-}
-
-function testTinkoff() {
-    var url = 'https://journal.tinkoff.ru/sell-apartment-buy-bonds/';
-    VCBR = journal_tinkoff_ru(url)
-}
-
